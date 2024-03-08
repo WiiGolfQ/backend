d0ab3b4b0b3f377b78eea6416f8448839a880ccc
vncz14 <57498723+vncz14@users.noreply.github.com>
2024-03-08 02:44:02 -0500

    remove path functionality from get_secret

--- wgl_backend/settings.py
+++ wgl_backend/settings.py
@@ -5,9 +5,6 @@ load_dotenv()
 
 def get_secret(key, default):
     value = os.getenv(key, default)
-    if os.path.isfile(value):
-        with open(value) as f:
-            return f.read()
     return value
 
 
